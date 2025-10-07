c73b7832368a1ebf83b496274f829e3eb39b687c
Lucas280908 <lucasrivolta15@gmail.com>
2025-10-07 08:58:39 -0300

    update

---
+++ publicacao.js
@@ -0,0 +1,15 @@
+// Botão que abre popup
+const botao = document.getElementById("BotaoPublicar");
+const popup = document.getElementById("TelaPublicacao");
+
+// Abrir
+botao.addEventListener("click", () => {
+    popup.style.display = "flex";
+});
+
+// Fechar clicando fora
+popup.addEventListener("click", (e) => {
+    if (e.target === popup) {
+        popup.style.display = "none";
+    }
+});
