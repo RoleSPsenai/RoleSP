8795cf54b467e3057e3223b84b8d7eaa7bfa29ff
Qu31R0zZ <allan754@outlook.com>
2025-12-03 10:58:28 -0300

    :construction: aligning the close button

--- script/publicacao.js
+++ script/publicacao.js
@@ -14,25 +14,6 @@ popup.addEventListener("click", (e) => {
     }
 });
 
-// SELECT de filtros
-
-let select = document.querySelector('.select'),
-    optionsFiltersLabel = document.getElementById('optionsFiltersLabel'),
-    selectValue = document.getElementById('selectValue'),
-    option = document.querySelectorAll('.option input');
-
-option.forEach(input => {
-    input.addEventListener('click', event => {
-        selectValue.textContent = input.dataset.lable;
-
-        const isMouseClick =
-        event.pointerType == 'mouse' ||
-        event.pointerType == 'touch';
-
-       isMouseClick && optionsFiltersLabel.click();
-})
-});
-    
 
     
      
