4441cb9b451ab0bcd42cabc0ed8f42cea6d83df8
Lucas280908 <lucasrivolta15@gmail.com>
2025-12-05 09:35:47 -0300

    :tada: First Commit

---
+++ tela-configuracao.js
@@ -0,0 +1,35 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    const botaoConfig = document.getElementById('configuracao');
+    const modalConfig = document.getElementById('config-modal');
+    const botaoFechar = document.getElementById('btn-fechar-config');
+
+    // Abrir popup
+    botaoConfig.addEventListener('click', () => {
+        modalConfig.showModal();
+    });
+
+    // Fechar popup
+    botaoFechar.addEventListener('click', () => {
+        modalConfig.close();
+    });
+
+});
+
+      // Obtém referências para os elementos
+        const fileInput = document.getElementById('fileInput');
+        const openExplorerLink = document.getElementById('openExplorerLink');
+
+        // Adiciona um ouvinte de evento de clique ao link
+        openExplorerLink.addEventListener('click', function(event) {
+            event.preventDefault(); // Impede o comportamento padrão do link (navegar para #)
+            fileInput.click(); // Simula um clique no input de arquivo oculto
+        });
+
+        // Opcional: Você pode adicionar um ouvinte de evento 'change' ao input para saber quando um arquivo foi selecionado
+        fileInput.addEventListener('change', function() {
+            if (this.files && this.files.length > 0) {
+                console.log('Arquivo selecionado:', this.files[0].name);
+                alert('Você selecionou o arquivo: ' + this.files[0].name);
+            }
+        });
