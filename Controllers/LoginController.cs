d413947b278c3ca018348fc4f2a48ea7778f14ff
Queiroz <Queiroz>
2025-12-10 21:00:15 -0300

    :label: Changing the Id erroCadastro

--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -6,8 +6,55 @@ using Sistema_Login.Service;
 
 namespace RoleSP.Controllers
 {
-    public class LoginController : Controller
-    {
-     
-    }
+    // public class LoginController : Controller
+    // {
+    //  private readonly AppDbContext _context;
+
+    //     public LoginController(AppDbContext context)
+    //     {
+    //         _context = context;
+    //     }
+
+    //     public IActionResult Index()
+    //     {
+    //         return View();
+    //     }
+
+    //     [HttpPost]
+    //     public IActionResult Entrar(string email, string senha)
+    //     {
+    //         if (string.IsNullOrWhiteSpace(email) || string.IsNullOrWhiteSpace(senha))
+    //         {
+    //             ViewBag.Error = "Preencha todos os campos.";
+    //             return View("Index");
+    //         }
+
+    //         byte[] senhaDigitadaHas = HashService.GerarHashBytes(senha);
+
+    //         var usuario = _context.Usuarios.FirstOrDefault(e => e.Email == email);
+    //         if(usuario == null)
+    //         {
+    //             ViewBag.Erro = "E-mail ou senha incorretos.";
+    //             return View("Index");
+    //         }
+
+    //         if(!usuario.Senha.SequenceEqual(senhaDigitadaHas))
+    //         {
+    //             ViewBag.Erro = "E-mail ou senha incorretos.";
+    //             return View("Index");
+    //         }
+
+    //         HttpContext.Session.SetString("UsuarioNome", usuario.NomeCompleto);
+    //         HttpContext.Session.SetInt32("UsuarioId",usuario.IdUsuario);
+
+    //         return RedirectToAction("Home", "index");
+    //     }
+
+    //     public IActionResult Sair()
+    //     {
+    //         HttpContext.Session.Clear();
+    //         return RedirectToAction("Index");
+    //     }
+
+    // }
 }
--- obj/Debug/net9.0/RoleSP.AssemblyInfo.cs
+++ obj/Debug/net9.0/RoleSP.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RoleSP")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a8c68ca08333b91ad7609a1d4425ec95ce2b3de")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb14ac4a52f2a6aa830e86cf6794ef32b04e7cd6")]
 [assembly: System.Reflection.AssemblyProductAttribute("RoleSP")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RoleSP")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
