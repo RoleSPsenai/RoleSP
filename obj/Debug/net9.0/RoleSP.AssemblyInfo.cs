28a38aa939c32ab4234a5d3be78a082445681e4b
MARIANA CORDEIRO DA SILVA <mariana.c.silva16@aluno.senai.br>
2025-12-15 10:57:42 -0300

    criação do arquivo GaleriaViewModel.

--- obj/Debug/net9.0/RoleSP.AssemblyInfo.cs
+++ obj/Debug/net9.0/RoleSP.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RoleSP")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+113f82173f1100965544868a957ecdef56df3b7c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a1f4ca6ff59110cef83237c7e57cfbc3587ab2c3")]
 [assembly: System.Reflection.AssemblyProductAttribute("RoleSP")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RoleSP")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
