7b4638256450e4c8c8a28f941790103aebb3002a
Qu31R0zZ <allan754@outlook.com>
2025-12-16 11:30:01 -0300

    Finalização

--- obj/Debug/net9.0/RoleSP.AssemblyInfo.cs
+++ obj/Debug/net9.0/RoleSP.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RoleSP")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+646f0428e2dea0dee856ce9a9e3c1e20e3ac2b7d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea97e249fd3228c7ac570cc8e5daa5b0ec149295")]
 [assembly: System.Reflection.AssemblyProductAttribute("RoleSP")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RoleSP")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
